838e288c239c079efcbd7db953fdb1f4fa7c1065
Tomi Martino Affandi <tomimartino10@gmail.com>
2024-09-28 10:33:46 +0700

    soal no 1 - 6

---
+++ P5T2/fungsiParameter.php
@@ -0,0 +1,15 @@
+<?php
+function perkenalan ($nama, $salam) {
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+perkenalan ("Hamdana", "Hallo");
+
+echo "<hr>";
+
+$saya = "Elok";
+$ucapanSalam = "Selamat pagi";
+
+perkenalan ($saya, $ucapanSalam);
+?>
