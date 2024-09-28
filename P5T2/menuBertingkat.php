d437ec8bce6ba14a6486faeb9067dccbaa5cc16b
Tomi Martino Affandi <tomimartino10@gmail.com>
2024-09-28 10:34:12 +0700

    soal no 7 - 8

--- P5T2/menuBertingkat.php
+++ P5T2/menuBertingkat.php
@@ -8,34 +8,42 @@ $menu = [
         "SubMenu" => [
             [
                 "nama" => "Wisata",
-                "SubMenu" => 
-                [
-                    "nama" => "Pantai"
-                ],
-                [
-                    "nama"=> "Gunung"
+                "SubMenu" => [
+                    [
+                        "nama" => "Pantai"
+                    ],
+                    [
+                        "nama" => "Gunung"
+                    ]
                 ]
+            ],
+            [
+                "nama" => "Kuliner"
+            ],
+            [
+                "nama" => "Hiburan"
             ]
-        ],
-        [
-            "nama" => "Kuliner"
-        ],
-            "nama" => "Hiburan"
+        ]
     ],
     [
         "nama" => "Tentang"
     ],
     [
         "nama" => "Kontak"
-    ],
+    ]
 ];
 
 function tampilkanMenuBertingkat(array $menu) {
     echo "<ul>";
-    foreach ($menu as $key => $item) {
-        echo "<li>{$item['nama']}</li>";
+    foreach ($menu as $item) {
+        echo "<li>{$item['nama']}";
+        if (isset($item['SubMenu'])) {
+            tampilkanMenuBertingkat($item['SubMenu']);
+        }
+        echo "</li>";
     }
     echo "</ul>";
 }
+
 tampilkanMenuBertingkat($menu);
 ?>
