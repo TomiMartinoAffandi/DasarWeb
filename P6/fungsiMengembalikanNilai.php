5f83ff512cdc99f5bdb900e301fa88e167378fb8
Tomi Martino Affandi <tomimartino10@gmail.com>
2024-10-04 09:24:59 +0700

    fungsi dan menu bertingkat soal no 4 - 12

---
+++ P6/fungsiMengembalikanNilai.php
@@ -0,0 +1,10 @@
+<?php
+
+function hitungUmur ($thn_lahir, $thn_sekarang) {
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+echo "Umur saya adalah ". hitungUmur (2004, 2024)."tahun" 
+//isi sesuai dengan tahun lahir kalian
+
+?>
---
+++ P6/menuBertingkat.php
@@ -0,0 +1,49 @@
+<?php
+$menu = [
+    [
+        "nama" => "Beranda"
+    ],
+    [
+        "nama" => "Berita",
+        "SubMenu" => [
+            [
+                "nama" => "Wisata",
+                "SubMenu" => [
+                    [
+                        "nama" => "Pantai"
+                    ],
+                    [
+                        "nama" => "Gunung"
+                    ]
+                ]
+            ],
+            [
+                "nama" => "Kuliner"
+            ],
+            [
+                "nama" => "Hiburan"
+            ]
+        ]
+    ],
+    [
+        "nama" => "Tentang"
+    ],
+    [
+        "nama" => "Kontak"
+    ]
+];
+
+function tampilkanMenuBertingkat(array $menu) {
+    echo "<ul>";
+    foreach ($menu as $item) {
+        echo "<li>{$item['nama']}";
+        if (isset($item['SubMenu'])) {
+            tampilkanMenuBertingkat($item['SubMenu']);
+        }
+        echo "</li>";
+    }
+    echo "</ul>";
+}
+
+tampilkanMenuBertingkat($menu);
+?>
