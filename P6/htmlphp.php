09371589552e021752a9ae93464da7835a28cf57
Tomi Martino Affandi <tomimartino10@gmail.com>
2024-10-04 09:43:08 +0700

    string dan entities

---
+++ P6/htmlphp.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>cara 01</title>
+    </head>
+    <body>
+        <p>tanggal hari ini : <?php echo date("d M Y")?></p>
+        <?php 
+            echo '<html>';
+            echo
+            '<head><title>Cara02</title></head>';
+            echo '<body>';
+            echo '<p>Tanggal Hari ini: '.date('d M Y'). '</p>';
+            echo '</body>';
+            echo '</html>';
+        ?>
+    </body>
+</html>
