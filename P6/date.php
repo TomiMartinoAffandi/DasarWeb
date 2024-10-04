2111d9722f8a6cd3985003e406bc017d9eed6011
Tomi Martino Affandi <tomimartino10@gmail.com>
2024-10-04 10:43:20 +0700

    soal no 19 - 26

---
+++ P6/date.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML
+<html>
+    <head>
+    </head>
+    <body>
+        <h3> Date </h3>
+        <?php
+        echo"today is" . date("Y/m/d") ."<br>";
+        echo"today is" . date("Y.m.d") ."<br>";
+        echo"today is" . date("Y-m-d") ."<br>";
+        echo"today is" . date("1");
+        ?>
+    </body>
+</html>
