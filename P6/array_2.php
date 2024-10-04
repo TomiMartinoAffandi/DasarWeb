2b4e8f87cb145ea8ea9e901d2e69076fb763f107
Tomi Martino Affandi <tomimartino10@gmail.com>
2024-10-04 08:07:36 +0700

    soal no 1

---
+++ P6/array_2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    </head>
+    <style>
+        .Dosen{
+            
+        }
+    </style>
+    <body>
+    <?php
+        $Dosen = [
+            'nama' => 'Elok Nur Hamdana',
+            'domisili' => 'Malang',
+            'jenis kelamin' => 'Perempuan' ];
+            echo "Nama : {$Dosen ['nama']} <br>";
+            echo "Domisili : {$Dosen ['domisili']} <br>";
+            echo "Jenis Kelamin : {$Dosen ['jenis kelamin']} <br>";
+    ?>
+    </body>
+</html>
