f42ee784e6ea9a8c32ac10a11ee17af618eaab1b
Tomi Martino Affandi <tomimartino10@gmail.com>
2024-10-04 09:00:51 +0700

    soal no 2 - 5

--- P6/array_2.php
+++ P6/array_2.php
@@ -1,24 +1,50 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
-    </head>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Data Dosen</title>
     <style>
-        .Dosen{
-            
+        table {
+            width: 50%;
+            border-collapse: collapse;
+            margin: 20px 0;
+            font-size: 18px;
+            text-align: left;
+        }
+        th, td {
+            padding: 10px;
+            border: 1px solid #dddddd;
+        }
+        th {
+            background-color: #f2f2f2;
         }
     </style>
-    <body>
+</head>
+<body>
+
+<h2>Data Dosen</h2>
+
+<table>
+    <tr>
+        <th>Label</th>
+        <th>Informasi</th>
+    </tr>
     <?php
         $Dosen = [
-            'nama' => 'Elok Nur Hamdana',
-            'domisili' => 'Malang',
-            'jenis kelamin' => 'Perempuan' ];
-            echo "Nama : {$Dosen ['nama']} <br>";
-            echo "Domisili : {$Dosen ['domisili']} <br>";
-            echo "Jenis Kelamin : {$Dosen ['jenis kelamin']} <br>";
+            'Nama' => 'Elok Nur Hamdana',
+            'Domisili' => 'Malang',
+            'Jenis Kelamin' => 'Perempuan'
+        ];
+        
+        foreach ($Dosen as $label => $informasi) {
+            echo "<tr>";
+            echo "<td>" . $label . "</td>";
+            echo "<td>" . $informasi . "</td>";
+            echo "</tr>";
+        }
     ?>
-    </body>
+</table>
+
+</body>
 </html>
---
+++ P6/fungsi.php
@@ -0,0 +1,9 @@
+<?php
+function perkenalan () {
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Tomi<br/>"; //Tulis sesuai nama kalian
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+    perkenalan();
+    perkenalan();
+?>
