ce20b32c3618338a997856d747f8779147949a71
Tomi Martino Affandi <tomimartino10@gmail.com>
2024-10-19 16:41:45 +0700

    soal no 6 dan 7

--- P8/upload.php
+++ P8/upload.php
@@ -4,19 +4,21 @@ if(isset($_POST["submit"])){
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
     $maxsize = 3*1024*1024;
 
     if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize)
     {
         if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
             echo "File berhasil diunggah.<br>";
-            echo "<img src='$targetfile' width='200'>";
         }
         else{
             echo "Gagal mengunggah file.";
         }
 
+    }
+    else{
+        echo "file tidak valid atau melebihi kapasistas maksimum";
     }
 }
 ?>
